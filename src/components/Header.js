9fc303925fb697251117a047b2e5b2dba569924c
BlakeAghili <peopleofksmo@gmail.com>
2020-06-06 14:44:25 -0500

    Function Conversion from React Component.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,24 @@
 import React from 'react';
 
-export class Header extends React.Component{
-    render() {
-        return (
-            <header className='top'>
-                <h1>Catch
-                    <span className='ofThe'>
+function Header(props) {
+    return (
+        <header className='top'>
+            <h1>Catch
+                <span className='ofThe'>
                         <span className='of'>Of</span>
                         <span className='the'>The</span>
                     </span>
                 Day</h1>
-                <h3 className = 'tagline'><span>Fresh Daily</span></h3>
-            </header>
-        )
-    }
+            <h3 className = 'tagline'>
+                <span>{props.tagline}</span></h3>
+        </header>
+    );
 }
+
+/*export class Header extends React.Component{
+    render() {
+        Header(this.props)
+    }
+}*/
 
 export default Header;
