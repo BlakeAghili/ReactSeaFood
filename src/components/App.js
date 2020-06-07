b232544750470810d59822638aa2048d916a067e
BlakeAghili <peopleofksmo@gmail.com>
2020-06-07 13:05:54 -0500

    Added State for fish collection

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,21 @@ import Inventory from "./Inventory";
 import Order from "./Order";
 
 class App extends React.Component{
+    state = {
+        fishes: {},
+        order: {}
+    };
+
+    addFish = (fish) => {
+        console.log('adding a fish');
+        // 1: Take a COPY of existing state
+        const fishesCopy = {...this.state.fishes};
+        // 2: add the new fish to this copy.
+        fishesCopy[`fish${Date.now()}`] = fish;
+        // use built-in setState to update the state with this new copy.
+        this.setState({fishes: fishesCopy});
+    };
+
     render() {
         return (
             <div className='catch-of-the-day'>
@@ -11,7 +26,7 @@ class App extends React.Component{
                     <Header tagline='Fresh Daily Food'/>
                 </div>
                 <Order />
-                <Inventory />
+                <Inventory addFish = {this.addFish} />
             </div>
         )
     }
