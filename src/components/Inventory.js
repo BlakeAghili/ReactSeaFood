cdc4e691037da73afbd2862fa0675f904c1f4610
Blake Aghili <peopleofksmo@gmail.com>
2020-06-25 21:17:21 -0500

    Started OAuth setup, got complex, will finish that part later lol

--- src/components/Inventory.js
+++ src/components/Inventory.js
@@ -1,9 +1,18 @@
 import React from 'react';
 import AddFishForm from "./AddFishForm";
 import EditFishForm from "./EditFishForm";
+import Login from './Login'
+import firebase from "firebase";
+
 
 export class Inventory extends React.Component{
+    authenticate = (loginProvider) => {
+        alert(loginProvider);
+    }
+
     render() {
+        {/* return <Login authenticate={this.authenticate}/> */}
+
         return (
             <div className='inventory'>
                 <h2>Inventory</h2>
